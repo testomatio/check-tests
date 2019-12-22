389cb34fe1133f4a6fa00a3bac6fedd19d9b181c
Davert <davert.ua@gmail.com>
2019-12-22 06:03:50 +0200

    fixed parser

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const Decorator = require('./decorator');
 // command
 // RUNNER_TOOL_CACHE="/tmp" GITHUB_REF=refs/heads/master INPUT_FRAMEWORK=mocha INPUT_TESTS="example/mocha/**.js" node index.js
 const mainRepoPath = process.env.GITHUB_WORKSPACE;
-const headRepoPath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('head-path') || 'gh-head');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-
+    
+    const headRepoPath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('head-path') || 'gh-head');
     const pattern = core.getInput('tests', { required: true });
 
     if (!mainRepoPath) {
@@ -72,9 +72,9 @@ async function run() {
     console.log(`Added ${diff.added.length} tests, removed ${diff.missing.length} tests`);
     console.log(`Total ${stats.tests.length} tests`);
     
-  } 
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
+    console.error(error);
   }
 }
 
