e294ed81b4a775e0ef6316b718302cf5749a647a
Davert <davert.ua@gmail.com>
2019-12-23 05:56:11 +0200

    used links only for skipped tests

--- decorator.js
+++ decorator.js
@@ -50,7 +50,7 @@ class Decorator {
         list.push('* [~~' + escapeSpecial(test.name) + '~~]' + `(${fileLink}/${test.file}#L${test.line}) ⚠️ *skipped*`);
         continue;  
       }
-      list.push('* [' + escapeSpecial(test.name) + ']' + `(${fileLink}/${test.file}#L${test.line})`);
+      list.push('* ' + escapeSpecial(test.name));
     }
 
     function escapeSpecial(text, open = '`', close = '`') {
