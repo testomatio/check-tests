263c8554375b793f2863922e12d585b69c3289f8
vkeyboardv <32944269+vkeyboardv@users.noreply.github.com>
2021-04-12 21:18:36 +0300

    Add ability to parse jest.each syntax along with .skip, .only (#35) (#37)

--- example/jest/vue.spec.js
+++ example/jest/vue.spec.js
@@ -38,11 +38,15 @@ test('history mode', async () => {
   expect(pkg.dependencies).toHaveProperty('vue-router')
 })
 
-test(`empty test`, () => {
+test('file should exist', () => {
   expect(files['src/router/index.js']).toBeTruthy()
 })
 
-test.skip('use with Babel', async () => {
+it.each(['src/router/index.js'])('%i file should exist (it.each)', (n) => {
+  expect(files[n]).toBeTruthy();
+});
+
+test.skip('skip: use with Babel (test)', async () => {
   const { pkg, files } = await generateWithPlugin([
     {
       id: 'babel',
@@ -66,3 +70,36 @@ test.skip('use with Babel', async () => {
 
   expect(pkg.dependencies).toHaveProperty('vue-router')
 })
+
+it.skip('skip: use with Babel (it)', async () => {
+  const { pkg, files } = await generateWithPlugin([
+    {
+      id: 'babel',
+      apply: require('@vue/cli-plugin-babel/generator'),
+      options: {}
+    },
+    {
+      id: 'router',
+      apply: require('../generator'),
+      options: {}
+    }
+  ])
+
+  expect(files['src/router/index.js']).toBeTruthy()
+  expect(files['src/router/index.js']).toMatch('component: () => import')
+  expect(files['src/views/About.vue']).toBeTruthy()
+  expect(files['src/views/Home.vue']).toBeTruthy()
+  expect(files['src/App.vue']).toMatch('<router-link to="/">Home</router-link>')
+  expect(files['src/App.vue']).not.toMatch('<script>')
+  expect(files['src/App.vue']).toMatch('#nav a.router-link-exact-active')
+
+  expect(pkg.dependencies).toHaveProperty('vue-router')
+})
+
+it.each(['src/router/index.js']).skip('skip: %i file should exist (it.each)', (n) => {
+  expect(files[n]).toBeTruthy();
+});
+
+it.each(['src/router/index.js']).skip('skip: %i file should exist (test.each)', (n) => {
+  expect(files[n]).toBeTruthy();
+});
