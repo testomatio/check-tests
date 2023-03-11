846a0beb01e7c8b17429b3846b821fb09a544109
Vitaliy Mykhailiuk AQA <mihaylukvv@gmail.com>
2023-03-11 18:54:02 +0200

    TASK#93: I have added aditional tests to playwright_test.js(check annotations)

--- tests/playwright_test.js
+++ tests/playwright_test.js
@@ -80,4 +80,49 @@ describe('playwright parser', () => {
     const lastTest = tests[tests.length - 1];
     expect(lastTest.suites.length).to.eql(0);
   });
+
+  it('should parse playwright-js tests with skip() annotation', () => {
+    source = fs.readFileSync('./example/playwright/annotations.js').toString();
+    ast = jsParser.parse(source, { sourceType: 'unambiguous' });
+    const tests = playwrightParser(ast, '', source);
+
+    expect(tests[1].code.trim()).to.equal("test.skip('my skip test @first', async ({ page }) => {".trim());
+    expect(tests[1].name).to.equal('my skip test @first');
+    expect(tests[1].suites.length).to.eql(1);
+    expect(tests[1].skipped).to.be.true;
+  });
+
+  it('should parse playwright-js tests with fixme() annotation', () => {
+    source = fs.readFileSync('./example/playwright/annotations.js').toString();
+    ast = jsParser.parse(source, { sourceType: 'unambiguous' });
+    const tests = playwrightParser(ast, '', source);
+
+    expect(tests[2].code.trim()).to.equal("test.fixme('my fixme test @third', async ({ page }) => {".trim());
+    expect(tests[2].name).to.equal('my fixme test @third');
+    expect(tests[2].suites.length).to.eql(1);
+    expect(tests[2].skipped).to.be.true;
+  });
+
+  it('should parse playwright-ts tests with skip() & fixme() annotations', () => {
+    source = fs.readFileSync('./example/playwright/annotations.ts').toString();
+    const program = tsParser.parse(source, {
+      sourceType: 'unambiguous',
+      loc: true,
+      range: true,
+      tokens: true,
+    });
+    ast = {
+      program,
+      type: 'File',
+    };
+    const tests = playwrightParser(ast, '', source);
+
+    expect(tests[1].code).to.include("test.skip('my skip test @first");
+    expect(tests[1].name).to.equal('my skip test @first');
+    expect(tests[1].skipped).to.be.true;
+
+    expect(tests[2].code).to.include("test.fixme('my fixme test @third");
+    expect(tests[2].name).to.equal('my fixme test @third');
+    expect(tests[2].skipped).to.be.true;
+  });
 });
