8ba68794ac85dcff11c26d7c26e32cdc710cc51d
davert <davert.ua@gmail.com>
2023-05-13 04:00:38 +0300

    added types and API definition

--- src/analyzer.js
+++ src/analyzer.js
@@ -6,6 +6,10 @@ const Decorator = require('./decorator');
 
 let parser;
 
+/**
+ * @typedef {import('../types').Test} TestData
+ */
+
 class Analyzer {
   constructor(framework, workDir = '.') {
     this.workDir = workDir;
@@ -126,6 +130,11 @@ class Analyzer {
       if (process.env.TESTOMATIO_PREPEND_DIR) {
         fileName = path.join(process.env.TESTOMATIO_PREPEND_DIR, fileName);
       }
+
+      /**
+       * Assigns the array of TestData objects to the `tests` variable.
+       * @type {TestData[]}
+       */
       const testsData = this.frameworkParser(ast, fileName, source);
       debug(testsData);
       this.rawTests.push(testsData);
