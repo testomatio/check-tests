bb0b1a5e601422483cd88e9e37aa8c3fd55ca6ce
Oleksandr Pelykh <olexandr13@gmail.com>
2023-05-16 13:52:55 +0300

    fix test failure

--- src/updateIds/index.js
+++ src/updateIds/index.js
@@ -2,12 +2,12 @@ const { updateIdsCommon, cleanIdsCommon } = require('./updateIds');
 const { updateIdsNewman, cleanIdsNewman } = require('./updateIds-newman');
 
 function updateIds(testData, testomatioMap, workDir, opts) {
-  if (opts.framework === 'newman') return updateIdsNewman(testomatioMap, workDir, opts);
+  if (opts?.framework === 'newman') return updateIdsNewman(testomatioMap, workDir, opts);
   return updateIdsCommon(testData, testomatioMap, workDir, opts);
 }
 
 function cleanIds(testData, testomatioMap, workDir, opts) {
-  if (opts.framework === 'newman') return cleanIdsNewman(testomatioMap, workDir, opts);
+  if (opts?.framework === 'newman') return cleanIdsNewman(testomatioMap, workDir, opts);
   return cleanIdsCommon(testData, testomatioMap, workDir, opts);
 }
 
