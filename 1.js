ed971a38370e646a011e3a92541c1484a1e9ef49
Oleksandr Pelykh <olexandr13@gmail.com>
2023-09-30 14:25:07 +0300

    Delete 1.js

--- 1.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const upath = require('upath');
-
-const path1 = 'foo/bar/baz';
-const path2 = 'foo\\bar\\baz';
-
-console.log(upath.toUnix(path1));
-console.log(upath.toUnix(path2));
