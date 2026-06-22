9fc5cc43831ac77ee9ffc672cea46293c7fa0dff
Denys Kuchma <148628428+DenysKuchma@users.noreply.github.com>
2026-06-22 20:47:25 +0300

    Update error reporting (#273)

--- src/reporter.js
+++ src/reporter.js
@@ -4,6 +4,7 @@ const debug = require('debug')('testomatio:ids');
 const { request } = isHttps ? require('https') : require('http');
 const path = require('path');
 const fs = require('fs');
+const { formatErrorMessage } = require('./lib/utils');
 const { TEST_ID_REGEX } = require('./updateIds/constants');
 
 class Reporter {
@@ -61,7 +62,17 @@ class Reporter {
             debug('Files fetched from Testomat.io', message);
             if (resp.statusCode !== 200) {
               debug('Files fetch failed', resp.statusCode, resp.statusMessage, message);
-              rej(message);
+              const error = new Error(
+                formatErrorMessage({
+                  statusCode: resp.statusCode,
+                  statusMessage: resp.statusMessage,
+                  body: message,
+                }),
+              );
+              error.statusCode = resp.statusCode;
+              error.statusMessage = resp.statusMessage;
+              error.body = message;
+              rej(error);
             } else {
               res(JSON.parse(message));
             }
